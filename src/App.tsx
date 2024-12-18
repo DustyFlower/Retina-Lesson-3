10a76c2941bd8983bf363c6691b61df47b688af3
DustyFlower <lukashevanatalia1992@gmail.com>
2024-12-18 16:27:48 +0100

    Adaptation images for Retina displays

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import './App.css';
-import image from './assets/images/image.jpg';
-
-
+import cat from './assets/images/cat.jpg';
+import cat2x from './assets/images/cat@2x.png';
+import cat3x from './assets/images/cat@3x.png';
 
 function App() {
     return (
         <div className="App">
             <p>Hello, Retina &#128075;</p>
-            {/* <div className={'bg'}></div> */}
-            {/* <img src={image} alt="nature"/> */}
+{/*             <div className={'bg'}></div>*/}
+             <img src={cat} srcSet={`${cat2x} 2x, ${cat3x} 3x` } alt="cat"/>
+
+            <picture>
+                <source srcSet={` ${cat3x} 3x, ${cat2x} 2x, ${cat}`}/>
+                <img src={cat} alt="cat"/>
+            </picture>
+
+            <figure>
+                <img src={cat} srcSet={`${cat2x} 2x, ${cat3x} 3x`} alt="cat"/>
+                <figcaption>Super cat</figcaption>
+            </figure>
         </div>
     );
 }
